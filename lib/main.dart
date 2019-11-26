9ca93db886bb81a628dfce085866297fc10ed331
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-11-26 01:57:39 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,106 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import 'package:flutter/material.dart';
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Retrieve Text Input',
+//      home: MyCustomForm(),
+//    );
+//  }
+//}
+//
+//// Define un widget de formulario personalizado
+//class MyCustomForm extends StatefulWidget {
+//  @override
+//  _MyCustomFormState createState() => _MyCustomFormState();
+//}
+//
+//// Define la clase State correspondiente. Esta clase contendrá los datos relacionados con
+//// nuestro formulario.
+//class _MyCustomFormState extends State<MyCustomForm> {
+//  // Crea un controlador de texto. Lo usaremos para recuperar el valor actual
+//  // del TextField!
+//  final myController = TextEditingController();
+//
+//  @override
+//  void dispose() {
+//    // Limpia el controlador cuando el Widget se descarte
+//    myController.dispose();
+//    super.dispose();
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('Retrieve Text Input'),
+//      ),
+//      body: Padding(
+//        padding: const EdgeInsets.all(16.0),
+//        child: TextField(
+//          controller: myController,
+//        ),
+//      ),
+//      floatingActionButton: FloatingActionButton(
+//        // Cuando el usuario pulsa el botón, muestra un diálogo de alerta con el
+//        // texto que el usuario ha digitado en nuestro campo de texto.
+//        onPressed: () {
+//          return showDialog(
+//            context: context,
+//            builder: (context) {
+//              return AlertDialog(
+//                // Recupera el texto que el usuario ha digitado utilizando nuestro
+//                // TextEditingController
+//                content: Text(myController.text),
+//              );
+//            },
+//          );
+//        },
+//        tooltip: 'Show me the value!',
+//        child: Icon(Icons.text_fields),
+//      ),
+//    );
+//  }
+//}
+
 ////Código para AlertDialog, el método
 //
 //import 'package:flutter/material.dart';
@@ -99,22 +199,8 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Uso de layouts en flutter
-
+////Uso de layouts en flutter
+//
 //import 'package:flutter/material.dart';
 //import 'package:english_words/english_words.dart';
 //
@@ -164,14 +250,35 @@
 //                child: const Text('Enabled Button',
 //                    style: TextStyle(fontSize: 20)),
 //              ),
-//
 //            ],
 //          ),
+//        ),
+//        floatingActionButton: FloatingActionButton(
+//          // Cuando el usuario pulsa el botón, muestra un diálogo de alerta con el
+//          // texto que el usuario ha digitado en nuestro campo de texto.
+//          onPressed: () {
+//            return showDialog(
+//              context: context,
+//              builder: (context) {
+//                return AlertDialog(
+//                  // Recupera el texto que el usuario ha digitado utilizando nuestro
+//                  // TextEditingController
+//                  content: Text("Hola mundo", style: descTextStyle),
+//                );
+//              },
+//            );
+//          },
+//          tooltip: 'Show me the value!',
+//          child: Icon(Icons.text_fields),
 //        ),
 //      ),
 //    );
 //  }
 //}
+
+
+
+
 
 //import 'package:flutter/material.dart';
 //import 'package:english_words/english_words.dart';
