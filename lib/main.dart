cf89d3125539ee2f111a6b39d012d658a5cba894
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-11-26 01:44:16 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,103 @@
 
 
 
+////Código para AlertDialog, el método
+//
+//import 'package:flutter/material.dart';
+//
+//void main() => runApp(MyApp());
+//
+///// This Widget is the main application widget.
+//class MyApp extends StatelessWidget {
+//  static const String _title = 'Flutter Code Sample';
+//
+//
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: _title,
+//      home: Scaffold(
+//        appBar: AppBar(title: const Text(_title)),
+//        body: MyStatelessWidget(),
+//      ),
+//    );
+//  }
+//}
+//
+///// This is the stateless widget that the main application instantiates.
+//class MyStatelessWidget extends StatelessWidget {
+//  MyStatelessWidget({Key key}) : super(key: key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Column(
+//        mainAxisSize: MainAxisSize.min,
+//        children: <Widget>[
+//          const RaisedButton(
+//            onPressed: null,
+//            child: Text('Disabled Button', style: TextStyle(fontSize: 20)),
+//          ),
+//          const SizedBox(height: 30),
+//          RaisedButton(
+//            onPressed: () {
+//              // [OK]We can show dialog.
+//              showMyDialog(context, "Este mensaje");
+//            },
+//            child: const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+//          ),
+//          const SizedBox(height: 30),
+//          RaisedButton(
+//            onPressed: () {},
+//            textColor: Colors.white,
+//            padding: const EdgeInsets.all(0.0),
+//            child: Container(
+//              decoration: const BoxDecoration(
+//                gradient: LinearGradient(
+//                  colors: <Color>[
+//                    Color(0xFF0D47A1),
+//                    Color(0xFF1976D2),
+//                    Color(0xFF42A5F5),
+//                  ],
+//                ),
+//              ),
+//              padding: const EdgeInsets.all(10.0),
+//              child:
+//              const Text('Gradient Button', style: TextStyle(fontSize: 20)),
+//            ),
+//          ),
+//        ],
+//      ),
+//    );
+//  }
+//
+//
+//  void showMyDialog(BuildContext context, String texto) {
+//    showDialog<bool>(
+//      context: context,
+//      builder: (BuildContext context) {
+//        return AlertDialog(
+//          content: Text(
+//            texto,
+//          ),
+//          actions: <Widget>[
+//            FlatButton(
+//              child: const Text('OK'),
+//              onPressed: () {
+//                Navigator.of(context).pop(true);
+//              },
+//            ),
+//          ],
+//        );
+//      },
+//    );
+//  }
+//
+//
+//}
+
+
 
 
 
