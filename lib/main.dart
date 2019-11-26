e7ba34f1fbd44ae7fc0b0dd31b1b0590d72844f1
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-11-26 02:27:27 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,62 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'InkWell Demo';
 
+    return MaterialApp(
+      title: title,
+      home: MyHomePage(title: title),
+    );
+  }
+}
 
+class MyHomePage extends StatelessWidget {
+  final String title;
 
+  MyHomePage({Key key, this.title}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            Center(child: MyButton()),
+            Center(child: MyButton()),
+            Center(child: MyButton()),
+          ],
+        ),
+      )
+//      Center(child: MyButton()),
+    );
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // El InkWell envuelve nuestro widget flat button personalizado
+    return InkWell(
+      // Cuando el usuario pulsa el button, muestra un snackbar
+      onTap: () {
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text('Texto'),
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.all(12.0),
+        child: Text('Flat Button'),
+      ),
+    );
+  }
+}
 
 //import 'package:flutter/material.dart';
 //
@@ -197,8 +222,6 @@
 //
 //}
 
-
-
 ////Uso de layouts en flutter
 //
 //import 'package:flutter/material.dart';
@@ -275,10 +298,6 @@
 //    );
 //  }
 //}
-
-
-
-
 
 //import 'package:flutter/material.dart';
 //import 'package:english_words/english_words.dart';
